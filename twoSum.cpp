614d0aed4fb10e359258d35fc48356ebd4f6b3f8
Vinicius Portácio <portaciopessoa@gmail.com>
2023-11-06 13:17:42 -0300

    Implementation

---
+++ twoSum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+
+class TwoSum {
+public: 
+  std::vector<int> sum(std::vector<int>& nums, int target) {   
+
+    for(int i = 0; i < nums.size(); i++) {
+      for(int j = 0; j < nums.size(); j++) {
+        if( nums[i] + nums[j] == target && i != j){
+          return {i, j};
+        }
+      }
+    }
+      return {};
+  };
+};
